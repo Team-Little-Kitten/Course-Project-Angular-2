f1f5c12e436b21f368b2ca5cd16619ba7d3ae9c7
belchevgb <belchev.gb@gmail.com>
2016-12-15 03:36:06 -0800

    Logout added

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
 
 import { AuthService } from './auth/auth.service';
 
@@ -8,11 +9,13 @@ import { AuthService } from './auth/auth.service';
 })
 export class AppComponent implements OnInit {
     private _authService: AuthService;
+    private _router: Router;
 
     public isUserLoggedIn: boolean;
 
-    constructor(authService: AuthService) {
+    constructor(authService: AuthService, router: Router) {
         this._authService = authService;
+        this._router = router;
         this.isUserLoggedIn = !!localStorage.getItem('username');
     }
 
@@ -20,5 +23,11 @@ export class AppComponent implements OnInit {
         this._authService
             .getIsUserLoggedIn()
             .subscribe((isLogged: boolean) => this.isUserLoggedIn = isLogged);
+    }
+
+    logout() {
+        localStorage.clear();
+        this._router.navigateByUrl('/');
+        this._authService.setIsUserLogged();
     }
 }
