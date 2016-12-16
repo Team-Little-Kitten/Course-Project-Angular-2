c4853a1cc3fa9fba504f348ce3094b15f7641cc6
belchevgb <belchev.gb@gmail.com>
2016-12-16 14:39:44 +0200

    Some files are minified

--- src/assets/js/custom.js
+++ src/assets/js/custom.js
@@ -1,10 +1,1 @@
-/* Write here your custom javascript codes */
-$(document).ready(function () {
-    App.init();
-    App.initCounter();
-    FancyBox.initFancybox();
-    OwlCarousel.initOwlCarousel();
-    OwlCarousel.initOwlCarousel2();
-    StyleSwitcher.initStyleSwitcher();
-    MasterSliderShowcase1.initMasterSliderShowcase1();
-});
+$(document).ready(function(){App.init(),App.initCounter(),FancyBox.initFancybox(),OwlCarousel.initOwlCarousel(),OwlCarousel.initOwlCarousel2(),StyleSwitcher.initStyleSwitcher(),MasterSliderShowcase1.initMasterSliderShowcase1()});
