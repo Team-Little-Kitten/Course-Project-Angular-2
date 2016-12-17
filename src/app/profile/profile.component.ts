078c1c1c0f3256ddeffd98f0c6028179bf0638d0
belchevgb <belchev.gb@gmail.com>
2016-12-17 15:50:45 +0200

    Added profile picture upload.

--- src/app/profile/profile.component.ts
+++ src/app/profile/profile.component.ts
@@ -1,14 +1,25 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { Http } from '@angular/http';
+import { IUser } from './user';
+import { UserService } from '../common-services';
 
 @Component({
     templateUrl: './profile.component.html',
     styleUrls: ['./profile.component.css']
 })
-export class ProfileComponent {
-    username: string;
+export class ProfileComponent implements OnInit {
+    public user: IUser;
 
-    constructor() {
-        this.username = JSON.parse(localStorage.getItem('user')).result.username;
+    private _userService: UserService;
+
+    constructor(userService: UserService) {
+        this._userService = userService;
+        this.user = JSON.parse(localStorage.getItem('user')).result;
+    }
+
+    public ngOnInit(): void {
+        this._userService
+            .getIsUserObjectUpdated()
+            .subscribe(() => this.user = JSON.parse(localStorage.getItem('user')).result);
     }
 }
