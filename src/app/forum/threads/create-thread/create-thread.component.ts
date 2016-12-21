874256c07feb012202e3915f0d0e1b79343e568b
Sparx92 <stefankoserkov@gmail.com>
2016-12-21 13:12:18 +0200

    Adding thread create page.

--- src/app/forum/threads/create-thread/create-thread.component.ts
+++ src/app/forum/threads/create-thread/create-thread.component.ts
@@ -1,19 +1,31 @@
-import { Component } from '@angular/core';
+import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
 
 @Component({
     templateUrl: './create-thread.component.html',
     styleUrls: ['./create-thread.component.css']
 })
-export class CreateThreadComponent {
+export class CreateThreadComponent implements OnInit {
     public createThreadForm: FormGroup;
     public fb: FormBuilder;
+    public options: Object;
+    public username: string;
 
-    constructor(fb: FormBuilder) {
-        this.fb = fb;
+    constructor(formBuilder: FormBuilder) {
+        this.fb = formBuilder;
+        this.username = JSON.parse(localStorage.getItem('user')).result.username;
+        this.options = { timeOut: 1500, pauseOnHover: true, showProgressBar: true, animate: 'scale', position: ['right', 'bottom'] };
+    }
+
+    ngOnInit() {
+        this.createThreadForm = this.fb.group({
+            title: ['Thred title', Validators.required],
+            content: ['Thread content', Validators.required],
+            author: [this.username]
+        });
     }
 
     public createThread(): void {
-        
+        console.log(this.createThreadForm.value);
     }
 }
--- src/app/forum/threads/threads.module.ts
+++ src/app/forum/threads/threads.module.ts
@@ -1,14 +1,15 @@
 import { NgModule } from '@angular/core';
 import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 import { CommonModule } from '@angular/common';
+import { SimpleNotificationsModule, NotificationsService } from './../../../../node_modules/angular2-notifications';
 
 import { ThreadComponent } from './thread.component';
 import { CreateThreadComponent } from './create-thread/create-thread.component';
 
 @NgModule({
     declarations: [ThreadComponent, CreateThreadComponent],
-    imports: [FormsModule, ReactiveFormsModule, CommonModule],
+    imports: [FormsModule, ReactiveFormsModule, CommonModule, SimpleNotificationsModule],
     exports: [ThreadComponent, CreateThreadComponent],
-    providers: []
+    providers: [NotificationsService]
 })
 export class ThreadsModule { }
