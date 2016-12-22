cf943915572a5557c2071ea3956cd030e34ec608
Sparx92 <stefankoserkov@gmail.com>
2016-12-22 14:29:16 +0200

    Disabled tinyMce.

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -2,7 +2,7 @@ import { NgModule } from '@angular/core';
 
 import { TinyEditorComponent } from './tiny-editor.component'
 @NgModule({
-    declarations: [TinyEditorComponent],
-    exports: [TinyEditorComponent]
+    // declarations: [TinyEditorComponent],
+    // exports: [TinyEditorComponent]
 })
 export class SharedModule { }
