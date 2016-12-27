f3f2c3815fd82a561481c8cf8e0884711c23d6f4
belchevgb <belchev.gb@gmail.com>
2016-12-27 13:00:06 +0200

    Minor forum changes.

--- src/app/forum/threads/create-thread/create-thread.component.ts
+++ src/app/forum/threads/create-thread/create-thread.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { FormGroup, FormControl, Validators, FormBuilder } from '@angular/forms';
-import { ThreadsService } from './../threads.service'
+import { ThreadsService } from './../threads.service';
+import { Router } from '@angular/router';
 
 import { NotificationsService } from './../../../../../node_modules/angular2-notifications';
 
@@ -13,21 +14,26 @@ export class CreateThreadComponent implements OnInit {
     public fb: FormBuilder;
     public options: Object;
     public username: string;
-
-    public pieceBodyText: string = "Magic";
+    public pieceBodyText: string;
 
     private _threadsService: ThreadsService;
     private _notificationService: NotificationsService;
+    private _router: Router;
 
-    constructor(formBuilder: FormBuilder, threadsService: ThreadsService, notificationService: NotificationsService) {
+    constructor(formBuilder: FormBuilder,
+        threadsService: ThreadsService,
+        notificationService: NotificationsService,
+        router: Router) {
+        this._router = router;
         this._notificationService = notificationService;
         this._threadsService = threadsService;
         this.fb = formBuilder;
         this.username = JSON.parse(localStorage.getItem('user')).result.username;
         this.options = { timeOut: 1500, pauseOnHover: true, showProgressBar: true, animate: 'scale', position: ['right', 'bottom'] };
+        this.pieceBodyText = "";
     }
 
-    ngOnInit() {
+    public ngOnInit(): void {
         this.createThreadForm = this.fb.group({
             title: ['Thred title', Validators.required],
             content: ['Thread content', Validators.required],
@@ -48,11 +54,11 @@ export class CreateThreadComponent implements OnInit {
         this._threadsService
             .postCreateThread(this.createThreadForm.value)
             .subscribe(response => {
-                console.log(response)
                 if (response.message.type === 'error') {
                     this._notificationService.error('Error', `${response.message.text}`);
                 } else {
-                    this._notificationService.success('success', `${response.message.text}`);
+                    this._notificationService.success('Success.', `Thread is created.`);
+                    setTimeout(() => this._router.navigateByUrl('/forum'), 1500);
                 }
             },
             err => console.log(err));
