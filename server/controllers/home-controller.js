0a072421305dbc95a595ad0d4b51cd458db6fd0e
Sparx92 <stefankoserkov@gmail.com>
2017-01-03 01:46:34 +0200

    Clean up

--- server/controllers/home-controller.js
+++ server/controllers/home-controller.js
@@ -4,11 +4,7 @@ module.exports = () => {
     return {
         loadHomePage(req, res) {
             console.log(req.user);
-            res.json(JSON.stringify({ "array": "arrrraaay" }))
-        },
-        test(req, res) {
-            console.log(req.user)
-            res.json(req.user);
+            res.json(JSON.stringify({ "message": "wellcome" }))
         }
     };
 };
--- server/routers/home-router.js
+++ server/routers/home-router.js
@@ -4,5 +4,4 @@ module.exports = ({ app, controllers }) => {
     const homeController = controllers.home;
 
     app.get("/", homeController.loadHomePage);
-    app.get("/test", homeController.test);
 };
